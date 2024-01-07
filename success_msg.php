961dca41aa37052e8c729aa58be23d99018fcc51
haekal2929 <155886984+haekal2929@users.noreply.github.com>
2024-01-07 19:30:07 +0800

    Add files via upload

---
+++ success_msg.php
@@ -0,0 +1,13 @@
+<style>
+    #uni_modal .modal-footer,#uni_modal .modal-header{
+        display:none
+    }
+</style>
+<div class="container-fluid">
+    <p>Permintaan Anda berhasil dikirim. Manajemen akan menghubungi Anda segera setelah mereka melihat permintaan Anda. Terima kasih!</p>
+    <div class="w-100 d-flex justify-content-end mx-2">
+        <div class="col-auto">
+            <button class="btn btn-dark btn-sm rounded-0" type="button" data-dismiss="modal">Close</button>
+        </div>
+    </div>
+</div>
